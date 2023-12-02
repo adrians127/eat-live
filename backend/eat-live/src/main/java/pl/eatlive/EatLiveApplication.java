cd70a737c91131f03433cd971ca244f50538ab6b
Adrian Stahl <adrianstahl.2@icloud.com>
2023-12-02 21:53:44 +0100

    create spring-boot project

---
+++ backend/eat-live/src/main/java/pl/eatlive/EatLiveApplication.java
@@ -0,0 +1,13 @@
+package pl.eatlive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatLiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EatLiveApplication.class, args);
+	}
+
+}
---
+++ backend/eat-live/src/test/java/pl/eatlive/EatLiveApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.eatlive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatLiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
